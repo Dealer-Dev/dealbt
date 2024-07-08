52983ce350a7abac0daaf76fd412022b47399c64
Dealer Services <159734968+Dealer-Dev@users.noreply.github.com>
2024-07-07 22:05:46 -0400

    Update http-server.py

--- Bot/http-server.py
+++ Bot/http-server.py
@@ -141,20 +141,20 @@ ID="$(cat ${FILE2}.name)" && ID="$(echo $ID | awk '{print $1}' | sed -e 's/[^0-9
 [[ ${ID} -lt '999' ]] && ID='576145089'
 TOKEN="$(cat /etc/ADM-db/token)"
 urlBOT="https://api.telegram.org/bot$TOKEN/sendMessage"
-MENSAJE="  =======================================\n"
-MENSAJE+=" ========📩𝙈𝙀𝙉𝙎𝘼𝙅𝙀 𝙍𝙀𝘾𝙄𝘽𝙄𝘿𝙊📩========\n"
-MENSAJE+=" =======================================\n"
+MENSAJE="━━━━━━━━━━━━━━━━━━━━━━━\n"
+MENSAJE+="━━𝙈𝙀𝙉𝙎𝘼𝙅𝙀 𝙍𝙀𝘾𝙄𝘽𝙄𝘿𝙊━━\n"
+MENSAJE+="━━━━━━━━━━━━━━━━━━━━━━━\n"
 MENSAJE+=" ${_key}\n"
-MENSAJE+=" =========== ☝️ USADA ☝ ============\n"
+MENSAJE+="━━KEY USADA━━\n"
 #MENSAJE+="            ☝️ USADA ☝️ \n"
 MENSAJE+=" API/KEY : ${RESELL}\n"
 MENSAJE+=" ID/API: ${ID} ✅ NOTIFICADO \n"
-MENSAJE+=" =======================================\n"
+MENSAJE+="━━━━━━━━━━━━━━━━━━━━━━━\n"
 MENSAJE+=" IP : $USRIP <-> HORA : $_hora\n"
-MENSAJE+=" =======================================\n"
-MENSAJE+='       🔰 Bot generador de key 🔰\n'
-MENSAJE+='           ⚜ By @ChumoGH ⚜ \n'
-MENSAJE+=" =======================================\n"
+MENSAJE+="━━━━━━━━━━━━━━━━━━━━━━━\n"
+MENSAJE+='         Bot generador de keys \n'
+MENSAJE+='            By @DealerServices235 \n'
+MENSAJE+="━━━━━━━━━━━━━━━━━━━━━━━\n"
 #curl -s -X POST $urlBOT -d chat_id=$ID -d text="$(echo -e "$MENSAJE")" &>/dev/null
 curl -s --max-time 10 -d "chat_id=$ID&disable_web_page_preview=1&text=$(echo -e "$MENSAJE")" $urlBOT &>/dev/null
 }
